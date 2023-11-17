496db70e947f010c446db156929583f0810a93fa
Awokay56 <kgbega56@gmail.com>
2023-11-17 06:53:28 -0800

    modified shell.c to follow c99 standard

--- shell.c
+++ shell.c
@@ -17,8 +17,6 @@ int main(int ac, char *argv[])
 	ssize_t read;
 	(void)ac;
 
-
-
 	while (1)
 	{
 		printf("#CISFUN$ ");
@@ -30,15 +28,24 @@ int main(int ac, char *argv[])
 		}
 		else if (read > 1)
 		{
+			char *command = line;
+			char **arguments;
+
 			if (line[read - 1] == '\n')
 				line[read - 1] = '\0';
 
-			char *command = line;
-			char *arguments[] = {command, NULL};
+		       arguments = malloc(2 * sizeof(char *));
+
+		       if (arguments == NULL)
+			       perror("malloc");
+
+
+		       arguments[0] = command;
+		       arguments[1] = NULL;
 
 			if (execve(command, arguments, NULL) == -1)
 				perror(argv[0]);
-
+			free(arguments);
 		}
 	}
 	free(line);
